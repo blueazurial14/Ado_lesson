33e962451e79f62e30f791a13c8533c075a433dc
blueazurial14 <dg.maud@gmail.com>
2019-07-01 13:55:32 +0200

    Création du projet

---
+++ ADO_lesson/Employee.sql
@@ -0,0 +1,30 @@
+﻿CREATE TABLE [dbo].[Employee]
+(
+	[Id] INT NOT NULL PRIMARY KEY,
+	[FirstName] NVARCHAR(50) NOT NULL,
+	[LastName] NVARCHAR(50) NOT NULL,
+	[BirthDate] DATETIME2 NOT NULL ,
+	[Email] NVARCHAR(50) NOT NULL,
+	[ResponsableID] INT NULL, 
+	CONSTRAINT [PK_Employee] Primary KEY ([Id]),
+	CONSTRAINT [UK_Employee_Email] UNIQUE ([Email]),
+	CONSTRAINT [CK_Employee_Birthdate] CHECK (YEAR(Birthdate)<2010),
+	CONSTRAINT [CK_Employee_Responsable] CHECK (ResponsableID <> Id),
+	CONSTRAINT [FK_Employee_Employee] FOREIGN KEY ([ResponsableID]) REFERENCES [Employee]([Id])
+
+)
+
+go
+
+CREATE INDEX [IX_Employee_LastName] on [dbo].[Employee] ([LastName])
+
+GO
+
+
+CREATE TRIGGER [dbo].[Trigger_OnlnsertEmployee]
+    ON [dbo].[Employee]
+    FOR DELETE, INSERT, UPDATE
+    AS
+    BEGIN
+        SET NoCount ON
+    END
---
+++ ADO_lesson/V_Employee.sql
@@ -0,0 +1,10 @@
+﻿CREATE VIEW [dbo].[V_Employee]
+	AS SELECT E.LastName ,
+	E.FirstName, 
+	E.BirthDate ,
+	EM.LastName as 'RespLastName' , 
+	EM.FirstName as 'RespFirstName' , 
+	E.Email,EM.Email as 'RespEmail' 
+
+
+	FROM Employee E join Employee EM on E.ResponsableID = EM.Id
